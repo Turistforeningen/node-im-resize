cab090f8ae24e73ed354e5f9e04c59c11af69601
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-11-14 22:42:16 +0100

    fix(jshint): strings must use singlequote

--- test.js
+++ test.js
@@ -151,7 +151,7 @@ describe('resize.cmd()', function() {
         suffix: '-1200',
         maxHeight: 1200,
         maxWidth: 1200,
-        aspect: "3:2"
+        aspect: '3:2'
       }]
     };
   });
@@ -297,47 +297,47 @@ describe('resize()', function() {
         suffix: '-1200',
         maxHeight: 1200,
         maxWidth: 1200,
-        aspect: "3:2"
+        aspect: '3:2'
       },{
         suffix: '-800',
         maxHeight: 800,
         maxWidth: 800,
-        aspect: "3:2"
+        aspect: '3:2'
       },{
         suffix: '-500',
         maxHeight: 500,
         maxWidth: 500,
-        aspect: "3:2"
+        aspect: '3:2'
       },{
         suffix: '-260',
         maxHeight: 260,
         maxWidth: 260,
-        aspect: "3:2"
+        aspect: '3:2'
       },{
         suffix: '-150',
         maxHeight: 150,
         maxWidth: 150,
-        aspect: "3:2"
+        aspect: '3:2'
       },{
         suffix: '-horizontal-500',
         maxHeight: 500,
         maxWidth: 500,
-        aspect: "3:2!h",
+        aspect: '3:2!h',
       },{
         suffix: '-vertical-500',
         maxHeight: 500,
         maxWidth: 500,
-        aspect: "3:2!v",
+        aspect: '3:2!v',
       },{
         suffix: '-square-200',
         maxHeight: 200,
         maxWidth: 200,
-        aspect: "1:1"
+        aspect: '1:1'
       },{
         suffix: '-square-50',
         maxHeight: 50,
         maxWidth: 50,
-        aspect: "1:1"
+        aspect: '1:1'
       }]
     };
   });
